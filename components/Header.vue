d41287484a5308d35ed99cd0e75371a7887588d0
pgw6541 <pgw6541@gmail.com>
2022-06-28 15:45:47 +0900

    Add nuxt Project

---
+++ components/Header.vue
@@ -0,0 +1,123 @@
+<template>
+  <header>
+    <Logo />
+    <div class="nav nav-pills">
+      <div
+        v-for="nav in navigations"
+        :key="nav.name"
+        class="nav-item">
+        <NuxtLink
+          :to="nav.href"
+          active-class="active"
+          :class="{ active: isMatch(nav.path) }"
+          class="nav-link"
+          exact>
+          {{ nav.name }}
+        </NuxtLink>
+      </div>
+    </div>
+    <div
+      class="user"
+      @click="toAbout">
+      <img
+        :src="image"
+        :alt="name" />
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Search',
+          href: '/'
+        },
+        {
+          name: 'Movie',
+          href: '/movie/tt4520988',
+          path: /^\/movie/
+        },
+        {
+          name: 'About',
+          href: '/about'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState('about', [
+      'image',
+      'name'
+    ])
+  },
+  methods: {
+    isMatch(path) {
+      if (!path) return false
+      return path.test(this.$route.fullPath)
+    },
+    toAbout() {
+      this.$router.push('/about')
+    }
+  },
+  head() {
+    return {
+      meta: [
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt Movie App' },
+        { hid: 'og:title', property: 'og:title', content: this.name },
+        { hid: 'og:description', property: 'og:description', content: this.email },
+        { hid: 'og:image', property: 'og:image', content: this.image },
+        { hid: 'og:url', property: 'og:url', content: `${process.env.CLIENT_URL}${this.$route.fullPath}` }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 40px;
+  position: relative;
+  .logo {
+    margin-right: 40px;
+  }
+  .user {
+    width: 40px;
+    height: 40px;
+    padding: 6px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 40px;
+    margin: auto;
+    transition: .4s;
+    &:hover {
+      background-color: darken($gray-200, 10%);
+    }
+    img {
+      width: 100%;
+    }
+  }
+  @include media-breakpoint-down(sm) {
+    .nav {
+      display: none;
+    }
+  }
+}
+</style>
